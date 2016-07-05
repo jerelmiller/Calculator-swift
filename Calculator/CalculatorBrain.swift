2cf0e008d0590849c9e6857b9b7317c86e046f46
Jerel Miller <jerelmiller@gmail.com>
2016-07-04 23:57:38 -0600

    Normalize values in dictionary

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -34,7 +34,7 @@ class CalculatorBrain {
     "÷": Operation.BinaryOperation({ $0 / $1 }),
     "+": Operation.BinaryOperation({ $0 + $1 }),
     "−": Operation.BinaryOperation({ $0 - $1 }),
-    "=": .Equals
+    "=": Operation.Equals
   ]
 
   var result: Double {
