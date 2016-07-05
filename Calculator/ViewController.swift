00597e1b1faa529735d6f799b111ca3bcd1f9e4c
Jerel Miller <jerelmiller@gmail.com>
2016-07-04 19:41:26 -0600

    Add PI button and add action for performing an operation

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-  @IBOutlet weak var display: UILabel?
+  @IBOutlet weak var display: UILabel!
 
   var userIsInTheMiddleOfTyping = false
 
@@ -17,11 +17,22 @@ class ViewController: UIViewController {
     let digit = sender.currentTitle!
 
     if userIsInTheMiddleOfTyping {
-      let textCurrentlyInDisplay = display!.text!
-      display!.text = textCurrentlyInDisplay + digit
+      let textCurrentlyInDisplay = display.text!
+      display.text = textCurrentlyInDisplay + digit
     } else {
-      display!.text = digit
-      userIsInTheMiddleOfTyping = true
+      display.text = digit
+    }
+
+    userIsInTheMiddleOfTyping = true
+  }
+
+  @IBAction func performOperation(sender: UIButton) {
+    userIsInTheMiddleOfTyping = false
+
+    if let mathematicalOperation = sender.currentTitle {
+      if mathematicalOperation == "π" {
+        display.text = String(M_PI)
+      }
     }
   }
 }
